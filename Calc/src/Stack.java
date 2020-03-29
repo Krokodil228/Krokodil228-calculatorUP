3dfe5c834fbfbb3b57781b906392c8977daca267
Krokodil228 <57894995+Krokodil228@users.noreply.github.com>
2020-03-29 22:13:21 +0700

    Add files via upload

---
+++ Calc/src/Stack.java
@@ -0,0 +1,27 @@
+//Непрерывная реализация стека символов.
+class Stack{
+    private static final int DEFSIZE = 16;
+    private char[] array;
+    private int head;
+
+    public Stack(){
+        array = new char[DEFSIZE];
+        head = 0;
+    }
+
+    public final void push(char c){
+        array[head++] = c;
+    }
+
+    public final char pop(){
+        return array[--head];
+    }
+
+    public final char top(){
+        return array[head-1];
+    }
+    public boolean hasnext(){
+        if(head>0) return true;
+        return false;
+    }
+}
